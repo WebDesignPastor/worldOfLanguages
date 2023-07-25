3f84c924c5c8ad75964381de0e1679f307c10c24
Francois PARMENTIER <webcreationtp@gmail.com>
2023-07-25 15:27:33 +0200

    Added model Language

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_132447) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_132656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.integer "difficulty_level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_profiles", force: :cascade do |t|
     t.integer "age"
