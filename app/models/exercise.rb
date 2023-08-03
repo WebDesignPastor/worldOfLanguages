6ad5ac603324b6ee8958592974dc577083a3f21c
Francois PARMENTIER <webcreationtp@gmail.com>
2023-08-03 16:30:25 +0200

    Continued validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,5 @@
 class Exercise < ApplicationRecord
   belongs_to :lesson
+
+  has_many :questions, as: :questionable
 end
--- app/models/lesson_test.rb
+++ app/models/lesson_test.rb
@@ -1,3 +1,5 @@
 class LessonTest < ApplicationRecord
   belongs_to :lesson
+
+  has_many :questions, as: :questionable
 end
