1f5974b6ca7662cdb5c8eea088e68b332ba4e9e7
Francois PARMENTIER$ <parmentier.corporate@gmail.com>
2023-08-03 05:27:06 +0200

    Finished model validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :language
+
+  has_many :lesson_content, dependent: :destroy
+  has_many :lesson_test, dependent: :destroy
+  has_many :exercise, dependent: :destroy
 end
