fad16125444f7108758def206ccd3c48e3a2b858
Francois PARMENTIER$ <parmentier.corporate@gmail.com>
2023-08-03 05:16:26 +0200

    Created user_profiles routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'user_profiles/new'
-  get 'user_profiles/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  resources :user_profiles, only: %i[show edit update]
   # root "articles#index"
 end
